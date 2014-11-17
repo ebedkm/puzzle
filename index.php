bbbfcc6be4a8c55ad12be601d1549cc9cc9abc95
ebed <ebed.kharistian@k24.co.id>
2014-11-17 13:05:14 +0700

    coba canvas

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <style>
+      body {
+        margin: 0px;
+        padding: 0px;
+      }
+    </style>
+  </head>
+  <body>
+    <canvas id="myCanvas" width="578" height="200"></canvas>
+    <script>
+      var image = new Object;
+      image.width = 200;
+      image.height = 137;
+
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+      var size = 2;
+      var Map = new Array;
+      for(i = 0; i < size; i++){
+        Map[i] = new Array;
+        for(j = 0; j < size; j++){
+          var box = new Object;
+            console.log(i + " " + j)
+            box.width = image.width/size;
+            box.height = image.height/size;
+            box.x = i*box.width;
+            box.y = j*box.height;
+
+            Map[i][j] = box;
+        }
+      }
+      console.log(Map[0][1].width);
+      for(i = 0; i < size; i++){
+        for(j = 0; j < size; j++){
+          console.log(i + " " + j )
+          var imageObj = new Image();
+          
+            context.drawImage(imageObj, Map[i][j].x, Map[i][j].y, Map[i][j].width, Map[i][j].height);
+          
+            imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+          }
+      }
+      
+    </script>
+  </body>
+</html>      
